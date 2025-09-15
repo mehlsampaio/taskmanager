0fa9d9699998ec26a83af4fdf57e21be5a2b8481
Mehl <mehlsampaio@gmail.com>
2025-09-15 17:30:53 -0300

    melhora legibilidade do código

--- src/main/java/com/example/taskmanager/model/Tarefa.java
+++ src/main/java/com/example/taskmanager/model/Tarefa.java
@@ -1,80 +1,22 @@
 package com.example.taskmanager.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import java.time.LocalDate;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Table(name = "TAREFA")
 public class Tarefa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
-
-    @Column(name = "nome", nullable = false, length = 60)
     private String nome;
-
-    @Column(name = "data_entrega", nullable = false, columnDefinition = "DATE")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dataEntrega;
-
-    @Column(name = "responsavel", nullable = false, length = 60)
     private String responsavel;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public LocalDate getDataEntrega() {
-        return dataEntrega;
-    }
-
-    public void setDataEntrega(LocalDate dataEntrega) {
-        this.dataEntrega = dataEntrega;
-    }
-
-    public String getResponsavel() {
-        return responsavel;
-    }
-
-    public void setResponsavel(String responsavel) {
-        this.responsavel = responsavel;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tarefa tarefa = (Tarefa) o;
-        return Objects.equals(id, tarefa.id) && Objects.equals(nome, tarefa.nome) && Objects.equals(dataEntrega, tarefa.dataEntrega) && Objects.equals(responsavel, tarefa.responsavel);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome, dataEntrega, responsavel);
-    }
-
-    @Override
-    public String toString() {
-        return "Tarefa{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", dataEntrega=" + dataEntrega +
-                ", responsavel='" + responsavel + '\'' +
-                '}';
-    }
 }
