cd6921385472a8c1ba151fb5dcca9abd858f8ea6
mehl <mehlsampaio@gmail.com>
2026-05-31 13:03:16 -0300

    docs: add swagger documentation config

--- src/main/java/io/github/mehlsampaio/taskmanager/TaskmanagerApplication.java
+++ src/main/java/io/github/mehlsampaio/taskmanager/TaskmanagerApplication.java
@@ -1,7 +1,18 @@
 package io.github.mehlsampaio.taskmanager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Taskmanager API",
+				version = "1.0",
+				description = "API REST para gerenciar tarefas"
+		)
+
+)
 
 @SpringBootApplication
 public class TaskmanagerApplication {
